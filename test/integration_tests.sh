72a1e3272238c65e1b0f121676c621ca4cb00b86
Rainer Poisel <rainer.poisel@gmail.com>
2020-08-31 06:19:02 +0200

    Build env version checks

--- test/integration_tests.sh
+++ test/integration_tests.sh
@@ -3,14 +3,8 @@
 pip3 install pytest
 pytest ../test/test_tcp.py
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]
-then
-  cat /etc/os-release | grep '^UBUNTU_CODENAME' | cut -d'=' -f2 | read _ UBUNTU_VERSION_NAME
-else
-  set UBUNTU_VERSION_NAME = "undefined";
-fi
-echo "Ubuntu version: ${UBUNTU_VERSION_NAME}"
-if [[ "$TRAVIS_OS_NAME" == "osx" || "$UBUNTU_VERSION_NAME" == "bionic" ]]
+read PYTHON_MAJOR PYTHON_MINOR PYTHON_PATCH <<< $(python3 --version | cut -d' ' -f2 | sed 's/\./ /g')
+if [[ (${PYTHON_MAJOR} -eq 3 && ${PYTHON_MINOR} -ge 6) ]]
 then
   pip3 install websockets
   pytest ../test/test_ws.py

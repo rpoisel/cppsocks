8f7b5988168718fbea5243e96f006a0da2c3ff63
Rainer Poisel <rainer.poisel@gmail.com>
2020-09-01 11:37:46 +0200

    WebSocketFrame interface and impl separated

--- src/http/socks_ws_frame.hpp
+++ src/http/socks_ws_frame.hpp
@@ -18,6 +18,8 @@ namespace Http
 class WebSocketFrame
 {
   public:
+  using CloseReasonCode = std::uint16_t;
+
   static constexpr std::uint8_t const OPCODE_CONTINUATION = 0x00;
   static constexpr std::uint8_t const OPCODE_TEXT = 0x01;
   static constexpr std::uint8_t const OPCODE_BINARY = 0x02;
@@ -47,133 +49,21 @@ class WebSocketFrame
     }
   }
 
-  static WebSocketFrame encode(Byte const* payload_, std::size_t payloadLength_, bool fin)
-  {
-    return WebSocketFrame(fin, OPCODE_BINARY, payload_, payloadLength_);
-  }
-
-  static WebSocketFrame encode(Byte const* payload_, std::size_t payloadLength_, bool fin, std::uint8_t opcode)
-  {
-    return WebSocketFrame(fin, opcode, payload_, payloadLength_);
-  }
-
-  static WebSocketFrame encode(char const* payload_, std::size_t payloadLength_, bool fin)
-  {
-    return WebSocketFrame(fin, OPCODE_TEXT, reinterpret_cast<Byte const*>(payload_), payloadLength_);
-  }
-
-  using CloseReasonCode = std::uint16_t;
-  static WebSocketFrame createConnectionClose(CloseReasonCode reasonCode = 0)
-  {
-    Byte payload_[sizeof(CloseReasonCode)];
-
-    hton(&payload_[0], reasonCode);
-
-    return WebSocketFrame(true, OPCODE_CONNECTION_CLOSE, &payload_[0], sizeof(payload_));
-  }
-
-  static WebSocketFrame createPong(Byte const* buf, std::size_t len)
-  {
-    return WebSocketFrame(true, OPCODE_CONNECTION_PONG, buf, len);
-  }
-
-  static bool decode(Byte const* buf, std::size_t len, WsBuffer& payloadBuf, std::uint8_t* opcode /* output */)
-  {
-    (void)len;
-
-    if (len < 2)
-    {
-      throw std::invalid_argument("WebSocket header is missing.");
-    }
-
-    std::size_t frameLen = (buf[1] & 0x7f);
-    bool mask = (buf[1] & 0x80) == 0x80;
-    *opcode = buf[0] & 0x0f;
-    std::size_t payloadOffset = 2;
-    // TODO check for correct len (must be > 2 now)
-    if (frameLen == 126)
-    {
-      frameLen = ntoh<std::uint16_t>(&buf[2]);
-      payloadOffset += 2;
-    }
-    else if (frameLen == 127)
-    {
-      frameLen = ntoh<std::uint64_t>(&buf[2]);
-      payloadOffset += 8;
-    }
-    auto oldPayloadSize = payloadBuf.size();
-    payloadBuf.resize(payloadBuf.size() + frameLen);
-
-    if (mask)
-    {
-      Byte const maskingKey[4]{buf[payloadOffset], buf[payloadOffset + 1], buf[payloadOffset + 2], buf[payloadOffset + 3]};
-      for (std::size_t cnt = 0; cnt < frameLen; cnt++)
-      {
-        payloadBuf[oldPayloadSize + cnt] = (buf[payloadOffset + 4 + cnt] ^ maskingKey[cnt % 4]);
-      }
-    }
-    else
-    {
-      std::memcpy(payloadBuf.data(), buf + payloadOffset, frameLen);
-    }
-
-    return (buf[0] & 0x80) == 0x80; /* value of FIN bit */
-  }
+  static WebSocketFrame encode(Byte const* payload_, std::size_t payloadLength_, bool fin);
+  static WebSocketFrame encode(Byte const* payload_, std::size_t payloadLength_, bool fin, std::uint8_t opcode);
+  static WebSocketFrame encode(char const* payload_, std::size_t payloadLength_, bool fin);
+  static WebSocketFrame createConnectionClose(CloseReasonCode reasonCode = 0);
+  static WebSocketFrame createPong(Byte const* buf, std::size_t len);
+  static bool decode(Byte const* buf, std::size_t len, WsBuffer& payloadBuf, std::uint8_t* opcode /* output */);
 
   Byte const* data() const { return data_.data(); }
   std::size_t length() const { return data_.size(); }
-
   Byte const* payload() const { return payloadLength() < 126 ? &data_[2] : &data_[4]; }
-  std::size_t payloadLength() const
-  {
-    auto frameLen = (data_[1] & 0x7f);
-    if (frameLen == 127)
-    {
-      throw std::runtime_error("Length > UINT16_MAX not supported yet.");
-    }
-    if (frameLen == 126)
-    {
-      return ntoh<std::uint16_t>(&data_[2]);
-    }
-    return frameLen;
-  }
-
+  std::size_t payloadLength() const;
   bool fin() const { return (data_[0] & 0x80) == 0x80; }
 
   private:
-  WebSocketFrame(bool fin, std::uint8_t opcode, Byte const* payload, std::size_t payloadLength) : data_{0, 0}
-  {
-    std::size_t headerSize = 2;
-    if (fin)
-    {
-      data_[0] = 0x80;
-    }
-    if (opcode > 15) // TODO: do not use magic number
-    {
-      throw std::invalid_argument("WebSocket opcode must not be bigger than 15.");
-    }
-    data_[0] |= opcode;
-
-    if (payloadLength < 126)
-    {
-      data_[1] |= payloadLength;
-    }
-    else if (payloadLength <= UINT16_MAX)
-    {
-      data_[1] |= 126;
-      headerSize += 2;
-      data_.resize(headerSize);
-      hton(&data_[2], static_cast<std::uint16_t>(payloadLength));
-    }
-    else
-    {
-      throw std::invalid_argument("Payload length > UINT16_MAX currently not supported.");
-    }
-
-
-    data_.resize(headerSize + payloadLength);
-    std::memcpy(data_.data() + headerSize, payload, payloadLength);
-  }
+  WebSocketFrame(bool fin, std::uint8_t opcode, Byte const* payload, std::size_t payloadLength);
 
   WsBuffer data_;
 };
@@ -181,5 +71,9 @@ class WebSocketFrame
 } // namespace Http
 } // namespace Network
 } // namespace Socks
+
+#if SOCKS_HEADER_ONLY == 1
+#include <socks_ws_frame.cpp>
+#endif /* SOCKS_HEADER_ONLY */
 
 #endif /* SOCKS_WS_FRAME_HPP */
